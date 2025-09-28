510c921c4d7ee799d587de8ebce04d076d116586
vikram kumar <123830628+vikramkumarsfe@users.noreply.github.com>
2025-09-28 08:26:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,25 @@ import Home from './components/Home';
 import Signup from './components/Signup';
 import Login from './components/Login';
 
-import StudentProfile from './components/Student/StudentProfile';
-import StudentResources from './components/Student/StudentResource';
-import StudentPayment from './components/Student/StudentPayment';
-import StudentLibrary from './components/Student/StudentLibrary';
-import StudentAttendance from './components/Student/StudentAttendance';
-import StudentDashboard from './components/Student/StudentDashboard';
-import StudentClasses from './components/Student/StudentClasses';
-import FacultyDashboard from './components/Faculty/FacultyDashboard';
-import FacultyClass from './components/Faculty/FacultyClasses';
-import FacultyDropoutPrediction from './components/Faculty/FacultyDropout';
-import FacultyLibrary from './components/Faculty/FacultyLibrary';
-import FacultyMarksUpdate from './components/Faculty/FacultyMarksUpdate';
-import FacultyResource from './components/Faculty/FacultyResource';
-import FacultyAttendance from './components/Faculty/FacultyAttendance';
+// Student Imports (Case-Correct)
+import StudentProfile from './components/student/StudentProfile';
+import StudentResources from './components/student/StudentResource';
+import StudentPayment from './components/student/StudentPayment';
+import StudentLibrary from './components/student/StudentLibrary';
+import StudentAttendance from './components/student/StudentAttendance';
+import StudentDashboard from './components/student/StudentDashboard';
+import StudentClasses from './components/student/StudentClasses';
+
+// Faculty Imports (Corrected to use lowercase 'faculty' directory)
+import FacultyDashboard from './components/faculty/FacultyDashboard';
+import FacultyClass from './components/faculty/FacultyClasses';
+import FacultyDropoutPrediction from './components/faculty/FacultyDropout';
+import FacultyLibrary from './components/faculty/FacultyLibrary'; // Case corrected
+import FacultyMarksUpdate from './components/faculty/FacultyMarksUpdate'; // Case corrected
+import FacultyResource from './components/faculty/FacultyResource'; // Case corrected
+import FacultyAttendance from './components/faculty/FacultyAttendance'; // Case corrected
+
+// Hostel Warden Imports (Case-Correct)
 import WardenDashboard from './components/HostelWarden/WardenDashboard';
 import WardenTotalRooms from './components/HostelWarden/WardenTotalRooms';
 import WardenProfile from './components/HostelWarden/WardenProfile';
@@ -34,6 +39,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        
+        {/* Student Routes */}
         <Route path='/student' element={<StudentDashboard/>}/>
         <Route path="/student/profile" element={<StudentProfile />} />
         <Route path="/student/resource" element={<StudentResources />} />
@@ -41,6 +48,8 @@ function App() {
         <Route path="/student/library" element={<StudentLibrary />} />
         <Route path="/student/attendance" element={<StudentAttendance />} />
         <Route path="/student/classes" element={<StudentClasses />} />
+        
+        {/* Faculty Routes */}
         <Route path='/faculty' element={<FacultyDashboard/>}/>
         <Route path='/faculty/classes' element={<FacultyClass/>}/>
         <Route path='/faculty/dropout' element={<FacultyDropoutPrediction/>}/>
@@ -48,6 +57,8 @@ function App() {
         <Route path='/faculty/marks' element={<FacultyMarksUpdate/>}/>
         <Route path='/faculty/resource' element={<FacultyResource/>}/>
         <Route path='/faculty/attendance' element={<FacultyAttendance/>}/>
+        
+        {/* Hostel Warden Routes */}
         <Route path='/hostel' element={<WardenDashboard/>}/>
         <Route path='/hostel/total-rooms' element={<WardenTotalRooms/>}/>
         <Route path='/hostel/profile' element={<WardenProfile/>}/>
